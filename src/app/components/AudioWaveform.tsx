b704860e02abfc15549d9a58602837b387b5f91d
Krisztian Hubner <krisztianhubner@gmail.com>
2024-03-02 16:53:58 +0100

    Updated drawWaveform to render both channels

--- src/app/components/AudioWaveform.tsx
+++ src/app/components/AudioWaveform.tsx
@@ -20,34 +20,78 @@ function drawWaveform(canvas: HTMLCanvasElement, audioBuffer: AudioBuffer) {
   canvas.width = canvasWidth;
   canvas.height = canvasHeight;
 
+  const halfHeight = canvasHeight / 2;
   const canvasCtx = canvas.getContext("2d")!;
   const buffer = audioBuffer.getChannelData(0);
 
+  const vScale = 1;
+
   canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
   canvasCtx.beginPath();
-  canvasCtx.strokeStyle = rose;
-  canvasCtx.lineWidth = 1;
 
-  const step = buffer.length / canvasWidth;
-  const amp = canvasHeight / 2;
-  for (let i = 0; i < canvasWidth; i++) {
-    let min = 1.0;
-    let max = -1.0;
-    for (let j = 0; j < step; j++) {
-      const dataIndex = Math.floor(i * step + j);
-      if (dataIndex < buffer.length) {
-        const value = buffer[dataIndex];
-        if (value < min) {
-          min = value;
-        } else if (value > max) {
-          max = value;
-        }
+  // Draws a polyline and fills it
+  function drawChannelPolyline(index: number) {
+    const channel = audioBuffer.getChannelData(index);
+    const channelLength = channel.length;
+    const sliceSize = canvasWidth / channelLength;
+    canvasCtx.moveTo(0, halfHeight);
+
+    let prevSlice = 0;
+    let maxValueInDataSlice = 0;
+
+    for (let i = 0; i < channelLength; i++) {
+      const currentSlice = Math.round(i * sliceSize);
+      if (currentSlice > prevSlice) {
+        const height = Math.round(maxValueInDataSlice * halfHeight * vScale);
+        const y = halfHeight + height * (index === 0 ? -1 : 1);
+        // console.log(`maxValueInDataSlice: ${maxValueInDataSlice}, height: ${height}`);
+
+        canvasCtx.lineTo(prevSlice, y);
+
+        prevSlice = currentSlice;
+        maxValueInDataSlice = 0;
+      }
+
+      const value = Math.abs(channel[i]);
+      if (value > maxValueInDataSlice) {
+        maxValueInDataSlice = value;
       }
     }
-    canvasCtx.moveTo(i, (1 + min) * amp);
-    canvasCtx.lineTo(i, (1 + max) * amp);
+
+    canvasCtx.lineTo(prevSlice, halfHeight);
   }
-  canvasCtx.stroke();
+
+  function drawSingleChannelLine() {
+    canvasCtx.strokeStyle = rose;
+    canvasCtx.lineWidth = 1;
+
+    const step = buffer.length / canvasWidth;
+    const amp = canvasHeight / 2;
+    for (let i = 0; i < canvasWidth; i++) {
+      let min = 1.0;
+      let max = -1.0;
+      for (let j = 0; j < step; j++) {
+        const dataIndex = Math.floor(i * step + j);
+        if (dataIndex < buffer.length) {
+          const value = buffer[dataIndex];
+          if (value < min) {
+            min = value;
+          } else if (value > max) {
+            max = value;
+          }
+        }
+      }
+      canvasCtx.moveTo(i, (1 + min) * amp);
+      canvasCtx.lineTo(i, (1 + max) * amp);
+    }
+    canvasCtx.stroke();
+  }
+
+  drawChannelPolyline(0);
+  drawChannelPolyline(1);
+
+  canvasCtx.fill();
+  canvasCtx.closePath();
 }
 
 export function AudioWaveform({ audioBuffer, width, height }: AudioWaveformProps) {
